4a5c344b0f4bec6c070a19cabb03bda3bf4896f4
howtheyknow <noreplyarain325@gmail.com>
2024-08-05 22:20:58 +0500

    Update config.ts

--- dev-cli/config.ts
+++ dev-cli/config.ts
@@ -1,12 +1,12 @@
 export function getConfig() {
-  let tmdbApiKey = process.env.MOVIE_WEB_TMDB_API_KEY ?? '';
+  let tmdbApiKey = process.env.MOVIE_WEB_TMDB_API_KEY ?? '1bb965af6888496c30d52a27e831f9c9';
   tmdbApiKey = tmdbApiKey.trim();
 
   if (!tmdbApiKey) {
     throw new Error('Missing MOVIE_WEB_TMDB_API_KEY environment variable');
   }
 
-  let proxyUrl: undefined | string = process.env.MOVIE_WEB_PROXY_URL;
+  let proxyUrl: undefined | string = "https://deluxe-unicorn-ddb80d.netlify.app/";
   proxyUrl = !proxyUrl ? undefined : proxyUrl;
 
   return {
